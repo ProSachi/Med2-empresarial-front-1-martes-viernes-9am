ed5318267e81692a47daaecd2b50ab0831184a6b
Prosachi <syosa@cesde.net>
2026-09-11 12:32:15 -0500

    Creación del repositorio

---
+++ Ejercicios/semana1/suma.js
@@ -0,0 +1,10 @@
+let numero1 = prompt("Ingrese el numero 1");
+let numero2 = parseInt(prompt("Ingrese el numero 2")); 
+/* let suma;
+suma = numero1 + numero2;
+console.log("La suma es: " + suma);
+console.log(`La suma es: ${suma} esa fue`);
+ */
+
+let respuesta = numero1 > 5 && numero1 < 18;
+console.log(typeof(numero1));
